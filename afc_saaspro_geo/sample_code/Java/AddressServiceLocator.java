ad040a0dd6315ae694a1e09a8bf9dd9b3b91a0e6
avalara-melissa-burnham <49682234+avalara-melissa-burnham@users.noreply.github.com>
2019-04-24 14:28:00 -0400

    more indentation fixes

--- afc_saaspro_geo/sample_code/Java/AddressServiceLocator.java
+++ afc_saaspro_geo/sample_code/Java/AddressServiceLocator.java
@@ -29,34 +29,34 @@ public class AddressServiceLocator {
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();			
 			con.setRequestMethod("POST");
-			 con.setRequestProperty("Content-Type","text/xml; charset=utf-8");
-			 con.setRequestProperty("SOAPAction", "http://tempuri.org/ILocatorService/GeocodeAddress");
-			 String xml = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">\r\n" + 
-			 		"	<s:Header>\r\n" + 
-			 		"		<o:Security s:mustUnderstand=\"1\" xmlns:o=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">\r\n" + 
-			 		"			<o:UsernameToken>\r\n" + 
-			 		"				<o:Username>***********</o:Username>\r\n" + 
-			 		"				<o:Password>***********</o:Password>\r\n" + 
-			 		"			</o:UsernameToken>\r\n" + 
-			 		"		</o:Security>\r\n" + 
-			 		"	</s:Header>\r\n" + 
-			 		"	<s:Body>\r\n" + 
-			 		"		<GeocodeAddress xmlns=\"http://tempuri.org/\">\r\n" + 
-			 		"			<inputAddress xmlns:a=\"http://schemas.datacontract.org/2004/07/EZGeoSaaS\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n" + 
-			 		"				<a:CassCertify>true</a:CassCertify>\r\n" + 
-			 		"				<a:City>Durham</a:City>\r\n" + 
-			 		"				<a:CityStateZip/>\r\n" + 
-			 		"				<a:MinimumScore>0.7</a:MinimumScore>\r\n" + 
-			 		"				<a:Offset>3</a:Offset>\r\n" + 
-			 		"				<a:Options>312</a:Options>\r\n" + 
-			 		"				<a:SecondaryUnit></a:SecondaryUnit>\r\n" + 
-			 		"				<a:State>NC</a:State>\r\n" + 
-			 		"				<a:StreetAddress>512 S Mangum St</a:StreetAddress>\r\n" + 
-			 		"				<a:Zip>27701</a:Zip>\r\n" + 
-			 		"			</inputAddress>\r\n" + 
-			 		"		</GeocodeAddress>\r\n" + 
-			 		"	</s:Body>\r\n" + 
-			 		"</s:Envelope>";
+			con.setRequestProperty("Content-Type","text/xml; charset=utf-8");
+			con.setRequestProperty("SOAPAction", "http://tempuri.org/ILocatorService/GeocodeAddress");
+			String xml = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:u=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd\">\r\n" + 
+			 	"	<s:Header>\r\n" + 
+			 	"		<o:Security s:mustUnderstand=\"1\" xmlns:o=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">\r\n" + 
+			 	"			<o:UsernameToken>\r\n" + 
+			 	"				<o:Username>***********</o:Username>\r\n" + 
+			 	"				<o:Password>***********</o:Password>\r\n" + 
+			 	"			</o:UsernameToken>\r\n" + 
+			 	"		</o:Security>\r\n" + 
+			 	"	</s:Header>\r\n" + 
+			 	"	<s:Body>\r\n" + 
+			 	"		<GeocodeAddress xmlns=\"http://tempuri.org/\">\r\n" + 
+			 	"			<inputAddress xmlns:a=\"http://schemas.datacontract.org/2004/07/EZGeoSaaS\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n" + 
+			 	"				<a:CassCertify>true</a:CassCertify>\r\n" + 
+			 	"				<a:City>Durham</a:City>\r\n" + 
+			 	"				<a:CityStateZip/>\r\n" + 
+			 	"				<a:MinimumScore>0.7</a:MinimumScore>\r\n" + 
+			 	"				<a:Offset>3</a:Offset>\r\n" + 
+			 	"				<a:Options>312</a:Options>\r\n" + 
+			 	"				<a:SecondaryUnit></a:SecondaryUnit>\r\n" + 
+			 	"				<a:State>NC</a:State>\r\n" + 
+			 	"				<a:StreetAddress>512 S Mangum St</a:StreetAddress>\r\n" + 
+			 	"				<a:Zip>27701</a:Zip>\r\n" + 
+			 	"			</inputAddress>\r\n" + 
+			 	"		</GeocodeAddress>\r\n" + 
+			 	"	</s:Body>\r\n" + 
+			 	"</s:Envelope>";
 			 con.setDoOutput(true);
 			 DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 			 wr.writeBytes(xml);
